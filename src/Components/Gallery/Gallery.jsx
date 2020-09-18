fba336f59578d9fe16c1f82c3a69310759916269
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2020-09-18 19:24:41 +0200

    Destructring the Iamge object

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -9,11 +9,7 @@ const Gallery = props => {
     // map variables to each item in fetched image array and return image component
     if (results.length > 0) {
         images = results.map(image => {
-            let farm = image.farm;
-            let server = image.server;
-            let id = image.id;
-            let secret = image.secret;
-            let title = image.title;
+            const {farm , server , id , secret , title} = image ;
             let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
 
             return <li><img src={url} key={id} alt={title} /></li>;
