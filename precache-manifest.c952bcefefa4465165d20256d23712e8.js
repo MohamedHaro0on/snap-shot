716f5bdec99a2cf2c100a5ec00902b93c06fc0e3
MohamedHaro0on <67293364+MohamedHaro0on@users.noreply.github.com>
2021-04-18 05:30:26 +0200

    Updates

---
+++ precache-manifest.c952bcefefa4465165d20256d23712e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e869a6ccfd8a0b46dcb175cc3d4dbc",
+    "url": "/snap-shot/index.html"
+  },
+  {
+    "revision": "1fbce5d8bc71391f979a",
+    "url": "/snap-shot/static/css/main.436d1005.chunk.css"
+  },
+  {
+    "revision": "f0a4fc965931a027a8a4",
+    "url": "/snap-shot/static/js/2.ffbedce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snap-shot/static/js/2.ffbedce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fbce5d8bc71391f979a",
+    "url": "/snap-shot/static/js/main.f2a45460.chunk.js"
+  },
+  {
+    "revision": "1c5a4bdd63d846c76c07",
+    "url": "/snap-shot/static/js/runtime-main.8c032352.js"
+  }
+]);
